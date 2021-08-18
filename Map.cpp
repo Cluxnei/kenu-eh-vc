e9266467edefa948866f8cc1194449b3a1cf82bd
= <=>
2021-08-18 17:31:32 -0300

    feature(refactor) - fix warnings & data casts

--- Map.cpp
+++ Map.cpp
@@ -1,30 +1,30 @@
 #include "Map.h"
 #include "SFML/Graphics.hpp"
 #include <cmath>
-#include <cstdlib>
 
 Map::Map(unsigned int unitSize, unsigned int width, unsigned int height)
 {
 	this->computeSquareCount(unitSize, width, height);
 	this->computeSquareSize(unitSize);
-	this->computeGridRectangleShapes(unitSize, width, height);
+	this->computeGrid(unitSize, width, height);
 	
 }
 
-void Map::computeGridRectangleShapes(unsigned int unitSize, unsigned int width, unsigned int height) {
+void Map::computeGrid(unsigned int unitSize, unsigned int width, unsigned int height) 
+{
 	const float size = static_cast<float>(unitSize);
-	const float leftToCenter = width / 2 - this->squareSize / 2;
-	const float topToCenter = height / 2 - this->squareSize / 2;
+	const float leftToCenter = floor((float)width / 2.f - (float)this->squareSize / 2.f);
+	const float topToCenter = floor((float)height / 2.f - (float)this->squareSize / 2.f);
 	const float gridCellOutline = 1.f;
 	const sf::Vector2f gridCellSize(size, size);
 
 	this->grid = new sf::RectangleShape * [this->squareCount];
 
-	for (int i = 0; i < this->squareCount; i++) {
+	for (unsigned int i = 0; i < this->squareCount; i++) {
 		this->grid[i] = new sf::RectangleShape[this->squareCount];
-		for (int j = 0; j < this->squareCount; j++) {
-			const float x = i * gridCellSize.x + gridCellOutline + leftToCenter;
-			const float y = j * gridCellSize.y + gridCellOutline + topToCenter;
+		for (unsigned int j = 0; j < this->squareCount; j++) {
+			const float x = i * gridCellSize.x + leftToCenter;
+			const float y = j * gridCellSize.y + topToCenter;
 			this->grid[i][j] = sf::RectangleShape(gridCellSize);
 			this->grid[i][j].setFillColor(sf::Color::Transparent);
 			this->grid[i][j].setOutlineColor(sf::Color::White);
@@ -34,32 +34,36 @@ void Map::computeGridRectangleShapes(unsigned int unitSize, unsigned int width, 
 	}
 }
 
-void Map::computeSquareCount(unsigned int unitSize, unsigned int width, unsigned int height) {
-	unsigned int horizontalUnitsCount = floor(
-		static_cast<float>(width) / static_cast<float>(unitSize)
-	) - 1;
-	unsigned int verticalUnitsCount = floor(
-		static_cast<float>(height) / static_cast<float>(unitSize)
-	) - 1;
+void Map::computeSquareCount(unsigned int unitSize, unsigned int width, unsigned int height) 
+{
+	const float u = static_cast<float>(unitSize);
+	const float w = static_cast<float>(width);
+	const float h = static_cast<float>(height);
 
-	this->squareCount = horizontalUnitsCount;
-	if (horizontalUnitsCount > verticalUnitsCount) {
-		this->squareCount = verticalUnitsCount;
-	}
+	unsigned int horizontalUnitsCount = static_cast<unsigned int>(floor(w / u)) - 1;
+	unsigned int verticalUnitsCount = static_cast<unsigned int>(floor(h / u)) - 1;
+
+	this->squareCount = horizontalUnitsCount > verticalUnitsCount 
+		? this->squareCount = verticalUnitsCount 
+		: horizontalUnitsCount;
 }
 
-void Map::computeSquareSize(unsigned int unitSize) {
+void Map::computeSquareSize(unsigned int unitSize) 
+{
 	this->squareSize = this->squareCount * unitSize;
 }
 
-unsigned int Map::getSquareSize() {
+unsigned int Map::getSquareSize() 
+{
 	return this->squareSize;
 }
 
-unsigned int Map::getSquareCount() {
+unsigned int Map::getSquareCount() 
+{
 	return this->squareCount;
 }
 
-sf::RectangleShape** Map::getGrid() {
+sf::RectangleShape** Map::getGrid() 
+{
 	return this->grid;
 }
--- Map.h
+++ Map.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+// #include <vector>
 
 class Map
 {
@@ -8,15 +9,17 @@ public:
 	unsigned int getSquareSize();
 	unsigned int getSquareCount();
 	sf::RectangleShape** getGrid();
+
 private:
 	void computeSquareCount(unsigned int unitSize, unsigned int width, unsigned int height);
 	void computeSquareSize(unsigned int unitSize);
-	void computeGridRectangleShapes(unsigned int unitSize, unsigned int width, unsigned int height);
+	void computeGrid(unsigned int unitSize, unsigned int width, unsigned int height);
 public:
 
 private:
 	sf::RectangleShape** grid;
 	unsigned int squareSize;
 	unsigned int squareCount;
+	// std::vector<sf::Vector2f> positionMap;
 };
 
